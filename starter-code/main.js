2ff0e867651dc0ffd0ac7bd81756c93a9fa08478
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-12 10:38:04 -0700

    added alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,4 +3,13 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-/*Testing Git push #2*/
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
