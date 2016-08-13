7672dd3db8248dae9557f033c03f765399fdcc22
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-12 20:11:07 -0700

    all code through unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,9 @@
-console.log("JS file is connected to HTML! Woo!")
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/* commenting this out for now:
 if (cardTwo === cardFour) {
 	alert("You found a match!");
 } else if (cardOne === cardTwo) {
@@ -13,3 +13,17 @@ if (cardTwo === cardFour) {
 } else {
 	alert("Sorry, try again.");
 }
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < 4; i++) {
+    	var cards = document.createElement('div');
+		cards.innerHTML;
+    	gameBoard.appendChild(cards);
+    	cards.className = 'card';
+	}
+}
+
+createBoard();
