cc55234083df69d1c593b54a45a1cce2e1e87461
Scott Whitman <scott.w.whitman@gmail.com>
2016-08-13 17:29:53 -0700

    Commit message

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,8 @@
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+*/
 /* commenting this out for now:
 if (cardTwo === cardFour) {
 	alert("You found a match!");
@@ -17,13 +17,42 @@ if (cardTwo === cardFour) {
 
 var gameBoard = document.getElementById('game-board');
 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
 var createBoard = function() {
-	for (var i = 0; i < 4; i++) {
-    	var cards = document.createElement('div');
-		cards.innerHTML;
-    	gameBoard.appendChild(cards);
-    	cards.className = 'card';
+	for (var i = 0; i < cards.length; i++) {
+    	var createCard = document.createElement('div');
+		createCard.innerHTML;
+    	gameBoard.appendChild(createCard);
+    	createCard.className = 'card';
+    	document.getElementsByClassName('card')[i].setAttribute('data-card', cards[i]);
+    	document.getElementsByClassName('card')[i].addEventListener('click', isTwoCards);
 	}
 }
 
 createBoard();
+
+var isMatch = function() {
+	return cardsInPlay[0] === cardsInPlay[1];
+	gameBoard.removeData();
+	createBoard();
+}
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay['data-card'] = 'king') {
+		cardElement.innerHTML = '<img src="king.png" alt="King of Spades" />';
+	} else {
+		cardElement.innerHTML = '<img src="queen.png" alt="Queen of Hearts" />';
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+
+
+
